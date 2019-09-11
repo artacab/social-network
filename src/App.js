24ff75a278903fc8d24fc4fa5c59bc87cd88a49e
artacab <artacab@yandex.ru>
2019-09-11 21:03:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
+    <div>
+      <Header/>
+      <Technologies/>
+    
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">asgwg</a>
+      <a href="#">assas</a>
+      <a href="#">24642rey</a>
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>dsa</li>
+        <li>useghjiers</li>
+        <li>erwhr</li>
+      </ul>
     </div>
   );
 }
