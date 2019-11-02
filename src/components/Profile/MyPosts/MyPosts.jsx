ef78330d5a517f632301ae59c75a4a3a9b434cd7
artacab <artacab@yandex.ru>
2019-11-02 13:07:59 +0300

    feat: good refactoring !

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,11 +10,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        props.dispatch({type: 'ADD-POST'});
     };
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+        props.dispatch(action);
     };
     return (
         <div>
@@ -35,7 +36,5 @@ const MyPosts = (props) => {
         </div>
     );
 
-
 };
-
 export default MyPosts;
