0d303a5e04a65881d19933334952b484992ec96a
artacab <artacab@yandex.ru>
2019-11-02 12:18:29 +0300

    feat: dispatch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './index.css';
-import state, {subscribe} from './redux/state';
+import store from './redux/state';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPostText} from './redux/state';
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
 
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} dispatch={store.dispatch.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 
