29a113c5f466985a95924d3ffa1be7c41da7c2bc
artacab <artacab@yandex.ru>
2019-11-03 14:17:58 +0300

    feat: add textarea for messages

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = (props) => {
                 <div className='app-wrapper-content'>
                     <Route path='/profile' render={() => <Profile profilePage={props.state.profilePage}
                                                                   dispatch={props.dispatch}/>}/>
-                    <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path='/dialogs' render={() => <Dialogs store={props.store}/>}/>
                 </div>
             </div>
         </BrowserRouter>
