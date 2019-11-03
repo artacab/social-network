3b6910be7f0f68c53864af25f593f8815fc81ce8
artacab <artacab@yandex.ru>
2019-11-03 19:06:54 +0300

    feat: reducers combine

--- src/redux/dialogs-reducer.jsx
+++ src/redux/dialogs-reducer.jsx
@@ -1,6 +1,24 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
-const dialogsReducer = (state, action) => {
+
+let initialState = {
+    messages: [
+        {id: 1, message: 'Yo'},
+        {id: 2, message: 'Qu'},
+        {id: 3, message: 'Hi'},
+        {id: 4, message: 'Ey'},
+        {id: 5, message: 'Boo'}
+    ],
+    dialogs: [
+        {id: 1, name: 'Artyom'},
+        {id: 2, name: 'Helen'},
+        {id: 3, name: 'Veronica'},
+        {id: 4, name: 'Ivan'},
+        {id: 5, name: 'Andrey'}
+    ],
+    newMessageBody: ''
+};
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY: {
             state.newMessageBody = action.body;
