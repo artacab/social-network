e07bdd026832b8557beca920045dc2fa0074835e
artacab <artacab@yandex.ru>
2019-12-25 12:45:19 +0300

    Updates

---
+++ precache-manifest.da13ccce83b7b1083b2127bf7a265dc1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c864ba6aa9482e367b21d89500b5a8",
+    "url": "/artacab/social-network/index.html"
+  },
+  {
+    "revision": "10298627828dd9b404f5",
+    "url": "/artacab/social-network/static/css/3.faf17327.chunk.css"
+  },
+  {
+    "revision": "5bf1bde595efc5d39032",
+    "url": "/artacab/social-network/static/css/4.5365c63b.chunk.css"
+  },
+  {
+    "revision": "b025d443a6215edbd773",
+    "url": "/artacab/social-network/static/css/main.891852ae.chunk.css"
+  },
+  {
+    "revision": "38157addf8d7c61ba4d0",
+    "url": "/artacab/social-network/static/js/2.ec21d0cc.chunk.js"
+  },
+  {
+    "revision": "10298627828dd9b404f5",
+    "url": "/artacab/social-network/static/js/3.a6e50ff4.chunk.js"
+  },
+  {
+    "revision": "5bf1bde595efc5d39032",
+    "url": "/artacab/social-network/static/js/4.f16b2b42.chunk.js"
+  },
+  {
+    "revision": "b025d443a6215edbd773",
+    "url": "/artacab/social-network/static/js/main.12c2ad11.chunk.js"
+  },
+  {
+    "revision": "d7c16eb4893c89c28206",
+    "url": "/artacab/social-network/static/js/runtime~main.1928db00.js"
+  },
+  {
+    "revision": "acf8252599df9c602a39a8602441fd60",
+    "url": "/artacab/social-network/static/media/loader.acf82525.svg"
+  },
+  {
+    "revision": "91cfe39100c061334019d31daf96fb62",
+    "url": "/artacab/social-network/static/media/user.91cfe391.png"
+  }
+]);
