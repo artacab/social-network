ae27b53d0b968248137ac327b148f9779ef3ffee
artacab <artacab@gmail.com>
2020-03-16 22:23:17 +0300

    feat: pathname

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -73,6 +73,12 @@ export const savePhoto = (file) => async (dispatch) => {
     if(response.data.resultCode === 0) {
         dispatch(savePhotoSuccess(response.data.data.photos));
     }
-
+}
+export const saveProfile = (profile) => async (dispatch, getState) => {
+    const userId = getState().auth.userId
+    let response = await profileAPI.saveProfile(profile);
+    if(response.data.resultCode === 0) {
+        dispatch(getUserProfile(userId));
+    }
 }
 export default profileReducer;
